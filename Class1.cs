d33b10c869145639787d61ef0c5a75ef882d7e61
Metritutus <324345+Metritutus@users.noreply.github.com>
2023-08-04 09:57:38 +0100

    Initial commit.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace ParenthesesNamespaceAdjustmentIssue001
+{
+    internal class Class1
+    {
+        private bool Condition1 { get; }
+        private bool Condition2 { get; }
+        private bool Condition3 { get; }
+
+        public void Method1()
+        {
+            if ((Condition1 && Condition2) || Condition3)
+            { }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace ParenthesesNamespaceAdjustmentIssue001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { }
+    }
+}
